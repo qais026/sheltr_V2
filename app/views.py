b684874f837dfe089eb6ed9d3f78e124070bbc23
qais026 <qais026@gmail.com>
2015-08-05 22:03:25 -0400

    App page updated with for loop

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'app/post_list.html', {})
+	# Example filter, display "posts" which is Post from a certain timezone and ordered by date
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'app/post_list.html', {'posts':posts})
