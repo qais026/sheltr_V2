b26bed8c26081de441f7e0266cfc8ce32615b94a
qais026 <qais026@gmail.com>
2015-08-06 21:31:48 -0400

    Detailed views per update, and basic CSS adds

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,7 @@ def post_list(request):
 	# Example filter, display "posts" which is Post from a certain timezone and ordered by date
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'app/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'app/post_detail.html', {'post': post})
