05b172a9875da8993e6b3a4540c4a941a9c437f5
qais026 <qais026@gmail.com>
2015-08-29 22:04:15 -0400

    Updated homepage graphics

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             name='Category',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=128, unique=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
                 ('slug', models.SlugField(unique=True)),
             ],
         ),
@@ -37,8 +37,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('provider_name', models.CharField(max_length=128)),
-                ('location_name', models.CharField(blank=True, max_length=128, null=True)),
-                ('website', models.CharField(blank=True, max_length=128, null=True)),
+                ('location_name', models.CharField(blank=True, null=True, max_length=128)),
+                ('website', models.CharField(blank=True, null=True, max_length=128)),
                 ('address1', models.CharField(blank=True, max_length=128)),
                 ('address2', models.CharField(blank=True, max_length=128)),
                 ('city', models.CharField(blank=True, max_length=128)),
