015482cb3b232c461df4afc0c51004a7486c314c
qais026 <qais026@gmail.com>
2015-08-29 22:16:54 -0400

    modified models to include notes

--- app/models.py
+++ app/models.py
@@ -43,6 +43,7 @@ class Provider(models.Model):
     contact = models.CharField(max_length=128, blank=True)
     phone = models.CharField(max_length=128, blank=True)
     hours = models.CharField(max_length=128, blank=True)
+    notes = models.Charfield(max_length=256, blank=True)
     category = models.ManyToManyField(Category, blank=True)
     def __str__(self):
         return self.provider_name
