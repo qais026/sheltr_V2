abf85f6fd396a9b718fe6ff2f30d21f1f0879e2c
tjiang11 <tjiang11@jhu.edu>
2015-09-12 02:11:03 -0400

    Added in filtering by category

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Category
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class SearchForm(forms.Form):
+	resourceType = forms.ModelMultipleChoiceField(queryset=None, label="Category")
+
+	def __init__(self, *args, **kwargs):
+		super(SearchForm, self).__init__(*args, **kwargs)
+		self.fields['resourceType'].queryset = Category.objects.all()
