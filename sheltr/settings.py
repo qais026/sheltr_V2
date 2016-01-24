266a2eddfdbc93b03c374ac46dcc02d19b36a416
Tony Jiang <Tony Jiang>
2016-01-24 15:48:12 -0600

    Change back to old database

--- sheltr/settings.py
+++ sheltr/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'sheltr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-        'NAME': os.path.join(BASE_DIR, 'db.test1'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
