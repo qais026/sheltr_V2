e10dc12b6e8c54954c51e675fdbbf4efc0dd692d
Tony Jiang <Tony Jiang>
2016-01-24 13:24:48 -0600

    Change to spatialite form sqlite database engine

--- sheltr/settings.py
+++ sheltr/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'sheltr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': os.path.join(BASE_DIR, 'db.sqatialite'),
     }
 }
 
