042b29193e616258f05dbb459f5de1733b4c2c6e
Tony Jiang <Tony Jiang>
2016-02-21 02:52:42 -0500

    Print response status code

--- app/helpers.py
+++ app/helpers.py
@@ -38,9 +38,12 @@ def get_lng_lat(location):
 def get_lat(location):
     location = urllib.parse.quote_plus(smart_str(location))
     url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % location
-    response = urllib.request.urlopen(url).read() 
+    #response = urllib.request.urlopen(url).read() 
+    response = requests.get(url)
     print(response.status_code)
-    result = json.loads(response.decode("utf-8"))
+    #result = json.loads(response.decode("utf-8"))
+    result = response.json()
+    
     if result['status'] == 'OK':
        return str(result['results'][0]['geometry']['location']['lat'])
     else:
