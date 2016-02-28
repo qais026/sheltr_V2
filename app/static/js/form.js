d7688a3c775f79e9a725e7c7cfacbc90e4d2cfeb
Tony Jiang <Tony Jiang>
2016-02-28 01:42:21 -0500

    semicolon

--- app/static/js/form.js
+++ app/static/js/form.js
@@ -64,7 +64,7 @@ $(function () {
 
 		distance = getDistanceFromLatLonInKm(provider_lat, provider_lng, ref_loc_lat, ref_loc_lng);
 
-		$('#provider-distance')[i].text(""+distance)
+		$('#provider-distance')[i].text(""+distance);
 	}
 
 	function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
