ea4ce01a49f6e6d7d98fdf25ce715efe03eb06d7
Tony Jiang <Tony Jiang>
2016-02-28 02:04:07 -0500

    test

--- app/static/js/form.js
+++ app/static/js/form.js
@@ -63,7 +63,9 @@ $(function () {
 		provider_lng = parseFloat(provider_latlng[1]);
 
 		distance = getDistanceFromLatLonInKm(provider_lat, provider_lng, ref_loc_lat, ref_loc_lng);
-		$('#provider-distance').text("test");
+
+		var provider_i = $('#provider-distance')[i];
+		$('#provider-distance').find(provider_i).text(distance);
 	}
 
 	function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
